caab2160c7f887f9658677ec954ea90014983958
mauriciohdeoliveira <mauriciohdeoliveira@gmail.com>
2022-03-13 19:21:50 -0300

    aula

---
+++ Aulas-Python/aula8.py
@@ -0,0 +1,14 @@
+"""
+Condicoes EFI, ELIF, ELSE - AULA 8
+"""
+
+if False:
+    print('Verdadeiro.')
+
+    num_1 = 2
+    num_2 = 4
+
+    print(num_1 + num_2)
+elif True:
+    nome = input('Qual seu nome? ')
+    print(f'seu nome nao é {nome}')
