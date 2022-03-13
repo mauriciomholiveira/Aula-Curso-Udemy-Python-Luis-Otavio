e324fb54d7fe4c4a800d55eb4d7e6f182d6849d5
Mauricio Oliveira <99220549+mauriciomholiveira@users.noreply.github.com>
2022-03-13 10:38:42 -0300

    Update Desafio-aula-6-1.py

--- Desafios-Python/Desafio-aula-6-1.py
+++ Desafios-Python/Desafio-aula-6-1.py
@@ -1,12 +1,34 @@
-"""
-Criar um codigo que pergunte o nome, a altura e o peso e informe o imc.
+#  Aqui perguntamos ao usuario alguns dados sobre o peso e altura
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite seu peso em Kg: "))
 
-"""
-
-nome = "Mauricio"
-idade = 25
-peso = 84
-altura = 1.70
+#  É feito o calculo do imc
 imc = peso / altura ** 2
 
-print('Ola', nome, 'voce tem', idade, 'e', peso, 'Kilos,', 'seu indice de massa corporal é: ', imc)
+#  Imprimos o IMC
+print("Seu IMC é: %.4f" % imc)
+
+# Caso abaixo dos 16 ele mostra esse resultado Magreza Grave
+if imc < 16:
+    print("Magreza grave")
+# Caso abaixo dos 17 ele mostra esse resultado Magreza Moderada
+elif imc < 17:
+    print("Magreza moderada")
+# Caso abaixo dos 18.5 ele mostra esse resultado Magreza Leve
+elif imc < 18.5:
+    print("Magreza leve")
+# Caso abaixo dos 16 ele mostra esse resultado Saudavel
+elif imc < 25:
+    print("Saudável")
+# Caso abaixo dos 18.5 ele mostra esse resultado Sobrepeso
+elif imc < 30:
+    print("Sobrepeso")
+# Caso abaixo dos 18.5 ele mostra esse resultado Obesidade Grau I
+elif imc < 35:
+    print("Obesidade Grau I")
+# Caso abaixo dos 18.5 ele mostra esse resultado Obesidade Grau II (severa)
+elif imc < 40:
+    print("Obesidade Grau II (severa)")
+# Caso abaixo dos 18.5 ele mostra esse resultado Obesidade Grau III (mórbida)
+else:
+    print("Obesidade Grau III (mórbida)")
